bf9a574f9183cd93c6c5d1f6513d5f13149a550f
Kasey <kasey_crocker@yahoo.com>
2022-10-25 17:00:29 -0400

    final commit

--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -11,17 +11,19 @@ const {
 } = require('../../controllers/userController');
 
 // /api/users GET all and POST 
-router.route('/').get(getUser).post(createUser);
+router.route('/')
+    .get(getUser)
+    .post(createUser);
 
 // /api/users/:userId/friends/:friendId POST and DELETE a friend by ID
 router.route('/:userId/friends/:friendId')
-.post(addFriend)
-.delete(deleteFriend);
+    .post(addFriend)
+    .delete(deleteFriend);
 
 // /api/users/:userId GET one user, PUT and DELETE by user's ID
 router.route('/:userId')
-.get(getSingleUser)
-.put(updateUser)
-.delete(deleteUser);
+    .get(getSingleUser)
+    .put(updateUser)
+    .delete(deleteUser);
 
 module.exports = router;
